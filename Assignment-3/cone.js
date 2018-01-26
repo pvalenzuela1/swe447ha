fc5c4aa7527555f2445f0f989e9aa27936b713e1
pvalenzuela1 <35516648+pvalenzuela1@users.noreply.github.com>
2018-01-25 21:24:01 -0800

    Create cone.js

---
+++ Assignment-3/cone.js
@@ -0,0 +1,26 @@
+var gl = null;
+var cone;
+
+function init() {
+    var canvas = document.getElementById( "webgl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+
+    if ( !gl ) {
+        alert("Unable to setup WebGL");
+        return;
+    }
+
+    gl.clearColor( 1.0, 0.0, 1.0, 1.0 );
+
+	cone = new Cone(gl, 100);
+    render();
+}
+
+function render() {
+	
+    gl.clear( gl.COLOR_BUFFER_BIT );
+	cone.render();
+}
+
+window.onload = init;
