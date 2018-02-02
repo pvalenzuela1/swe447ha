578127b9350e8a3b47624280fe13d1f9b8327354
pvalenzuela1 <35516648+pvalenzuela1@users.noreply.github.com>
2018-02-01 17:04:23 -0800

    Update Cube.js

--- Common/Cube.js
+++ Common/Cube.js
@@ -16,19 +16,39 @@ function Cube( vertexShaderId, fragmentShaderId ) {
         return; 
     }
 
-    this.positions = { 
+        this.positions = { 
         values : new Float32Array([
-           // Add your list vertex positions here
+           0.5, 0.5, 0.5, //v00
+		   0.5, 0.5, -0.5, //v01
+		   0.5, -0.5, 0.5, //v02
+		   0.5, -0.5, -0.5, //v03
+		   -0.5, 0.5, 0.5, //v04
+		   -0.5, 0.5, -0.5, //v05
+		   -0.5, -0.5, 0.5, //v06
+		   -0.5, -0.5, -0.5, //v07
+		   
             ]),
         numComponents : 3
     };
     
     this.indices = { 
         values : new Uint16Array([
-            // Add your list of triangle indices here
+			0, 1, 2,
+			1, 3, 2, 
+			2, 3, 7,
+			2, 7, 6,
+			0, 2, 6,
+			4, 0, 6,
+			3, 1, 7,
+			7, 1, 5,
+			6, 7, 4,
+			4, 7, 5,
+			4, 5, 0,
+			5, 1, 0,
         ])
     };
     this.indices.count = this.indices.values.length;
+
 
     
     this.positions.buffer = gl.createBuffer();
